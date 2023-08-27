1b9908b8904ea71b149b6567442f8c1b33431e0a
Leonard <leonardzhou2077@gmail.com>
2023-08-27 16:03:04 +0800

    update'

--- ProjectExecution/urls.py
+++ ProjectExecution/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
 urlpatterns = [
-    ]
+    path('create_project', project_views.create_project),
+    path('update_project', project_views.update_project),
+    path('delete_project', project_views.delete_project),
+]
--- ProjectExecution/views/project_views.py
+++ ProjectExecution/views/project_views.py
@@ -1,16 +1,21 @@
 from django.core.files.base import ContentFile
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from django.core.files.storage import default_storage
 from ProjectExecution.models import Project
 from ProjectExecution.serializers import ProjectSerializer
-import os
 
 from TeamManagement.models import Team
 
 
+@csrf_exempt
 @api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def create_project(request):
     data = request.data
 
@@ -38,4 +43,57 @@ def create_project(request):
             project.project_image.delete(save=False)  # 删除旧文件
             project.project_image.save(new_filename, new_file, save=True)  # 保存新文件
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@csrf_exempt
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def update_project(request):
+    try:
+        project_id = request.data.get('project_id')
+        project = Project.objects.get(project_id=project_id)
+    except Project.DoesNotExist:
+        return Response({'error': 'Project does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    team_id = request.data.get('team_id')
+    try:
+        team = Team.objects.get(team_id=team_id)
+    except Team.DoesNotExist:
+        return Response({'error': 'Team does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer = ProjectSerializer(project, data=request.data)
+
+    if serializer.is_valid():
+        serializer.validated_data['team'] = team
+        serializer.save()
+
+        # Rename and save image file
+        if 'project_image' in request.FILES:
+            old_file = project.project_image
+            new_filename = f"{project.project_id}_image.png"
+
+            new_file = ContentFile(old_file.read())
+            new_file.name = new_filename
+
+            project.project_image.delete(save=False)
+            project.project_image.save(new_filename, new_file, save=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@csrf_exempt
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def delete_project(request):
+    try:
+        project_id = request.data.get('project_id')
+        project = Project.objects.get(project_id=project_id)
+    except Project.DoesNotExist:
+        return Response({'error': 'Project does not exist'}, status=status.HTTP_404_NOT_FOUND)
+
+    project.delete()
+    return Response({'status': 'Project deleted'}, status=status.HTTP_204_NO_CONTENT)
