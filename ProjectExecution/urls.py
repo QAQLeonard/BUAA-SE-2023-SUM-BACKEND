cba044d1a1233183b92d0b6116ba99a939950469
Leonard <leonardzhou2077@gmail.com>
2023-08-27 17:13:53 +0800

    update

--- ProjectExecution/urls.py
+++ ProjectExecution/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('create_project', project_views.create_project),
     path('update_project', project_views.update_project),
     path('delete_project', project_views.delete_project),
+    path('get_project', project_views.get_team_projects),
 ]
