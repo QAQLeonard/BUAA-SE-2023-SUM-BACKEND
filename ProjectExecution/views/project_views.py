6b8ed78ece34bf1f2da59ed9c43d143d64bc2ebe
Leonard <leonardzhou2077@gmail.com>
2023-08-27 16:25:06 +0800

    debug

--- ProjectExecution/views/project_views.py
+++ ProjectExecution/views/project_views.py
@@ -12,88 +12,94 @@ from ProjectExecution.serializers import ProjectSerializer
 from TeamManagement.models import Team
 
 
-@csrf_exempt
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def create_project(request):
-    data = request.data
-
     try:
-        team = Team.objects.get(team_id=data.get('team_id'))
+        # 从请求数据中获取信息
+        data = request.data
+        team_id = data.get('team_id')
+        project_id = data.get('project_id')
+        project_name = data.get('project_name')
+        project_description = data.get('project_description')
+        project_image = request.FILES.get('project_image', None)
+
+        # 根据team_id获取Team对象
+        team = Team.objects.get(team_id=team_id)
+
     except Team.DoesNotExist:
         return Response({"error": "Team does not exist"}, status=status.HTTP_400_BAD_REQUEST)
 
-    # 使用序列化器验证数据
-    serializer = ProjectSerializer(data=data)
+    # 使用获取的数据创建新的Project对象
+    project = Project(
+        project_id=project_id,
+        project_name=project_name,
+        project_description=project_description,
+        team=team,
+        project_image=project_image
+    )
 
-    if serializer.is_valid():
-        serializer.validated_data['team'] = team
-        # 保存数据到数据库
-        project = serializer.save()
+    # 保存Project对象到数据库
+    project.save()
 
-        # 重命名并保存图片文件
-        if 'project_image' in request.FILES:
-            old_file = project.project_image
-            new_filename = f"{project.project_id}_image.png"
+    # 如果需要，你可以在这里进行额外的处理，比如图片文件重命名
+    if project_image:
+        new_filename = f"{project.project_id}_image.png"
 
-            new_file = ContentFile(old_file.read())
-            new_file.name = new_filename
+        new_file = ContentFile(project.project_image.read())
+        new_file.name = new_filename
 
-            project.project_image.delete(save=False)  # 删除旧文件
-            project.project_image.save(new_filename, new_file, save=True)  # 保存新文件
+        project.project_image.delete(save=False)  # 删除旧文件
+        project.project_image.save(new_filename, new_file, save=True)  # 保存新文件
 
     return Response({"status": "success", "message": "Project Created"}, status=status.HTTP_201_CREATED)
 
 
-@csrf_exempt
-@api_view(['POST'])
+@api_view(['PUT'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def update_project(request):
+    data = request.data
     try:
-        project_id = request.data.get('project_id')
-        project = Project.objects.get(project_id=project_id)
-    except Project.DoesNotExist:
-        return Response({"status": "error", "message": "Project does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        project = Project.objects.get(project_id=data.get('project_id'))
+        team = Team.objects.get(team_id=data.get('team_id'))
+    except (Project.DoesNotExist, Team.DoesNotExist):
+        return Response({"error": "Project or Team does not exist"}, status=status.HTTP_400_BAD_REQUEST)
 
-    team_id = request.data.get('team_id')
-    try:
-        team = Team.objects.get(team_id=team_id)
-    except Team.DoesNotExist:
-        return Response({"status": "error", "message": "Team does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+    # 更新数据
+    project.project_name = data.get('project_name', project.project_name)
+    project.project_description = data.get('project_description', project.project_description)
+    project.team = team
 
-    serializer = ProjectSerializer(project, data=request.data)
+    # 处理图片
+    if 'project_image' in request.FILES:
+        old_file = project.project_image
+        new_filename = f"{project.project_id}_image.png"
 
-    if serializer.is_valid():
-        serializer.validated_data['team'] = team
-        serializer.save()
+        new_file = ContentFile(old_file.read())
+        new_file.name = new_filename
 
-        # Rename and save image file
-        if 'project_image' in request.FILES:
-            old_file = project.project_image
-            new_filename = f"{project.project_id}_image.png"
+        project.project_image.delete(save=False)  # 删除旧文件
+        project.project_image.save(new_filename, new_file, save=True)  # 保存新文件
 
-            new_file = ContentFile(old_file.read())
-            new_file.name = new_filename
+    project.save()
 
-            project.project_image.delete(save=False)
-            project.project_image.save(new_filename, new_file, save=True)
-
-        return Response({"status": "success", "message": "Project Updated"}, status=status.HTTP_200_OK)
-    return Response({"status": "error", "message": "Unknown"}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"status": "success", "message": "Project Updated"}, status=status.HTTP_200_OK)
 
 
-@csrf_exempt
-@api_view(['POST'])
+@api_view(['DELETE'])
 @authentication_classes([TokenAuthentication])
 @permission_classes([IsAuthenticated])
 def delete_project(request):
+    project_id = request.data.get('project_id')
+
     try:
-        project_id = request.data.get('project_id')
         project = Project.objects.get(project_id=project_id)
     except Project.DoesNotExist:
-        return Response({"status": "error", "message": "Project does not exist"}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Project does not exist"}, status=status.HTTP_400_BAD_REQUEST)
 
     project.delete()
-    return Response({"status": "success", "message": "Project deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+    return Response({"status": "success", "message": "Project Deleted"}, status=status.HTTP_200_OK)
+
