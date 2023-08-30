aa23d1c1dec22dbe9100ac3217d69f32a96b80f1
Leonard <leonardzhou2077@gmail.com>
2023-08-30 20:51:47 +0800

    update

--- ProjectExecution/models.py
+++ ProjectExecution/models.py
@@ -51,7 +51,7 @@ class Prototype(models.Model):
 
 class Doc(models.Model):
     doc_id = models.CharField(max_length=40, primary_key=True)
-    doc_name = models.CharField(max_length=255)
+    doc_name = models.CharField(max_length=255, null=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     editable_by_guests = models.BooleanField(default=False)
 
