9c268f68ed2efdb7c9caf8e72926cdec77f4775e
Leonard <leonardzhou2077@gmail.com>
2023-09-02 19:36:29 +0800

    update

---
+++ server.js
@@ -0,0 +1,42 @@
+import axios from 'axios';  // 确保已经安装了Axios
+import { Server } from "@hocuspocus/server";
+import { Database } from "@hocuspocus/extension-database"
+
+const server = Server.configure({
+    port: 80,
+
+  async onConnect() {
+    console.log('🔮')
+  },
+
+  extensions: [
+    new Database({
+      fetch: ({ documentName }) => {
+        return new Promise((resolve, reject) => {
+          axios.get(`/api/v1/pe/docs/${documentName}/data/`)
+          .then(response => {
+            resolve(response.data.yjs_data); // 如果返回的字段是'yjs_data'，请相应地调整这里
+          })
+          .catch(error => {
+            console.error(error);
+            reject(null);
+          });
+        });
+      },
+      store: ({ documentName, state }) => {
+        return new Promise((resolve, reject) => {
+          axios.post(`/api/v1/pe/docs/${documentName}/data/`, { yjs_data: state }) // 根据后端视图来设置键名
+          .then(() => {
+            resolve();
+          })
+          .catch(error => {
+            console.error(error);
+            reject();
+          });
+        });
+      },
+    }),
+  ],
+});
+
+server.listen();
